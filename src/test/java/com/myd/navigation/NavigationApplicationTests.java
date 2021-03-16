521e93df5d8c934d136c6297afa7145343a2d0bf
OF3787 <mayuanding@qianmi.com>
2021-03-16 19:30:24 +0800

    导航项目

---
+++ src/test/java/com/myd/navigation/NavigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myd.navigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavigationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
